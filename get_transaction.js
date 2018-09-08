87b943ead69cca5257ec762710174c6997c145c0
Ethan Hunt <ethan@Ethans-MP.local>
2018-09-09 00:10:12 +0700

    Add more content for index

---
+++ get_transaction.js
@@ -0,0 +1,31 @@
+const _ = require('lodash')
+const Web3 = require('web3')
+const els = require('elasticsearch')
+const BigNumber = require('bignumber.js')
+const fs = require('fs');
+
+const web3 = new Web3()
+web3.setProvider(new web3.providers.HttpProvider("http://geth-node:8545"))
+
+web3.eth.getTransaction('0x984a873492fbbfbcc63652d9b95a1b181d82843ab441df40c7940fdc113482d5', (err, transaction) => {
+  console.log(transaction)
+  console.log(transaction.value)
+  console.log(transaction.gasPrice)
+  console.log('-------------------------------------------------')
+
+  let value = new BigNumber(transaction.value)
+  value = value.shiftedBy(-18)
+
+  let gasPrice = new BigNumber(transaction.gasPrice)
+  gasPrice = gasPrice.shiftedBy(-9)
+
+  console.log(value.toNumber())
+  console.log(value.toString())
+  console.log(gasPrice.toNumber())
+  console.log(gasPrice.toString())
+})
+
+web3.eth.getTransactionReceipt('0x984a873492fbbfbcc63652d9b95a1b181d82843ab441df40c7940fdc113482d5', (err, result) => {
+  console.log(result.gasUsed)
+  console.log(web3.toDecimal(result.status))
+})
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const _ = require('lodash')
 const Web3 = require('web3')
 const els = require('elasticsearch')
 const BigNumber = require('bignumber.js')
+const fs = require('fs');
 
 const web3 = new Web3()
 web3.setProvider(new web3.providers.HttpProvider("http://geth-node:8545"))
@@ -23,14 +24,15 @@ async function indexTransactions(currentBlock) {
       const start = new Date()
       console.log('Index transactions of block:', i)
       
-      const result = await web3.eth.getBlock(i, true)
+      const block = await web3.eth.getBlock(i, true)
 
-      for(var j = 0; j < result.transactions.length; j++) {
+      for(var j = 0; j < block.transactions.length; j++) {
         
-        const transaction = result.transactions[j]
-        
+        const transaction = block.transactions[j]
+
         const data = {}
-        const value = new BigNumber(transaction.value, 10).shiftedBy(-18)
+        const value = new BigNumber(transaction.value).shiftedBy(-18)
+        const gasPrice = new BigNumber(transaction.gasPrice).shiftedBy(-9)
   
         data.blockNumber = transaction.blockNumber
         data.blockHash = transaction.blockHash
@@ -39,14 +41,22 @@ async function indexTransactions(currentBlock) {
         data.to = transaction.to
         data.value = value.toNumber()
         data.valueStr = value.toString()
-  
+        data.gas = transaction.gas
+        data.gasPrice = gasPrice.toNumber()
+
+        const receipt = await web3.eth.getTransactionReceipt(transaction.hash)
+
+        data.gasUsed = receipt.gasUsed
+        data.status = web3.toDecimal(receipt.status)
+        data.timestamp = block.timestamp
+
         const existed = await client.exists({
           index: 'eth',
           type: '_doc',
           id: transaction.hash
         })
 
-        if (existed) return
+        if (existed) continue
 
         await client.create({
           index: 'eth',
@@ -54,8 +64,12 @@ async function indexTransactions(currentBlock) {
           id: transaction.hash,
           body: data
         })
+
+        fs.writeFileSync('./index.last', i, err => {
+          console.log(err)
+        })
       }
-      console.log('Index %d transactions of block %d in %s (ms)', result.transactions.length, i, new Date() - start)
+      console.log('Index %d transactions of block %d in %s (ms)', block.transactions.length, i, new Date() - start)
     } catch (err) {
       console.log("Occurs an error", err)
     }
